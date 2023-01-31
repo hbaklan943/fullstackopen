e2316f656e892df9f02eff3ae02d777042d1cb92
hbaklan943 <hbaklan943@gmail.com>
2023-01-31 23:52:35 +0300

    2.10

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,45 +1,61 @@
 import React from 'react'
 import { useState } from 'react'
+import Filter from './components/Filter'
+import Numbers from './components/Numbers'
+import PersonForm from './components/PersonForm'
 
 const App = () => {
 
   const [persons, setPersons] = useState([
-    { name: 'Harun' },
-    { name: 'Fevzi' }
+    { name: 'Harun', number: '5383758727' },
+    { name: 'Fevzi', number: '8714671871' },
+    { name: 'Keko', number: '7835698352' },
+    { name: 'hakan', number: '198468196' },
+    { name: 'Kıro', number: '1092747325' }
   ])
-  const [newName, setNewName] = useState('')
+  const [newPerson, setNewPerson] = useState({ name: '', number: '' })
+  const [searchInput, setSearchInput] = useState('')
+  console.log(persons.filter((person) => person.name === searchInput));
 
   const handleSubmit = (event) => {
     event.preventDefault();
-
-    if (persons.filter((person) => person.name === newName)[0]) {
-      alert(`${newName} Already in list`);
+    if (persons.filter((person) => (person.name === newPerson.name || person.number === newPerson.number)).length) {
+      alert(`${newPerson.name} Already in list`);
     }
+    else if (newPerson.name === '' || newPerson.number === '') alert("Empty entry")
     else {
-      setPersons(persons.concat(
-        { name: newName }
-      ))
+      setPersons(persons.concat(newPerson)
+      )
     }
-    setNewName('');
+    setNewPerson({ name: '', number: '' });
+  }
+
+  const handleNameChange = (event) => {
+    setNewPerson(
+      { ...newPerson, name: event.target.value }
+    )
 
   }
-  const handleNameChange = (event) => {
-    setNewName(event.target.value)
+  const handleNumberChange = (event) => {
+    setNewPerson(
+      { ...newPerson, number: event.target.value }
+    )
   }
+  const handleSearch = (event) => {
+    setSearchInput(event.target.value)
+  }
+
   return (
     <div>
-      <div>debug: {newName}</div>
       <h1>Phonebook</h1>
-      <form onSubmit={handleSubmit}>
-        <div>
-          name: <input value={newName} onChange={handleNameChange} />
-        </div>
-        <div>
-          <button type='submit'>add</button>
-        </div>
-      </form>
+      <Filter value={searchInput} onChange={handleSearch} />
+
+      <h2>Add a new</h2>
+      <PersonForm onSubmit={handleSubmit} newPersonName={newPerson.name} newPersonNumber={newPerson.number} onNameChange={handleNameChange} onNumberChange={handleNumberChange} />
+
       <h2>Numbers:</h2>
-      {persons.map((person) => <li key={person.name}>{person.name}</li>)}
+      <Numbers searchInput={searchInput} persons={persons} />
+
     </div>
   )
 }
