724ee79ffa678f39757549d95f2928fc34edb53e
hbaklan943 <hbaklan943@gmail.com>
2023-02-10 16:15:26 +0300

    exercise2.20

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -1,12 +1,13 @@
 import { useState } from 'react'
 import axios from 'axios'
 
-function Countrylist({ countries }) {
+function Countrylist({ countries, setCountries }) {
   return (
     <ul>
       {countries.map(country =>
         <li key={country.name.common}>
           {country.name.common}
+          <button onClick={() => setCountries([country])}>show</button>
         </li>
       )}
     </ul>
@@ -14,7 +15,19 @@ function Countrylist({ countries }) {
 }
 
 function Singlecountry({ countries }) {
+  const [temperature, setTemperature] = useState(null)
+  const [wind, setWind] = useState(null)
+  const [weatherIconLink, setWeatherIconLink] = useState('')
 
+  axios
+    .get(`https://api.weatherapi.com/v1/current.json?key=${process.env.REACT_APP_API_KEY}&q=${countries[0].capital[0]}`)
+    .then(response => {
+      setTemperature(response.data.current.temp_c)
+      setWind(response.data.current.wind_kph)
+      setWeatherIconLink(`http:${response.data.current.condition.icon}`)
+    })
+
+  //TODO condition not required
   return countries.length === 1
     ? (
       <ul>
@@ -25,12 +38,20 @@ function Singlecountry({ countries }) {
         <img src={countries[0].flags.png} />
         <h3>Languages:</h3>
         <ul>
-          {Object.entries(countries[0].languages).map(entry => <li>{entry[1]}</li>)}
+          {Object.entries(countries[0].languages).map(entry => <li key={entry[1]}>{entry[1]}</li>)}
         </ul>
+        <h3>Weather in {countries[0].capital[0]}: </h3>
+        <ul>
+          <li>temperature: {temperature}°C</li>
+          <img src={weatherIconLink} />
+          <li>wind: {wind} kph</li>
+        </ul>
+
       </ul>
     )
     : null
 }
+
 
 
 function App() {
@@ -57,14 +78,13 @@ function App() {
   }
 
 
-
-
-
   return (
     <div >
       <input onChange={search} value={input} />
-      <Countrylist countries={countries} />
-      <Singlecountry countries={countries} />
+      {countries.length === 1
+        ? <Singlecountry countries={countries} />
+        : <Countrylist countries={countries} setCountries={setCountries} />
+      }
     </div>
   );
 }
