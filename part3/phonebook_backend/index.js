8446adc551c2e4b10f9b236175b8d8f602e06eda
hbaklan943 <hbaklan943@gmail.com>
2023-02-11 17:19:08 +0300

    exercise3.4

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -2,7 +2,7 @@ const { request } = require('express')
 const express = require('express')
 const app = express()
 app.use(express.json())
-const phonebook = [
+let persons = [
     {
         "id": 1,
         "name": "Arto Hellas",
@@ -26,7 +26,7 @@ const phonebook = [
 ]
 
 app.get('/api/persons', (request, response) => {
-    response.json(phonebook)
+    response.json(persons)
 })
 
 app.get('/api/info', (request, response) => {
@@ -34,6 +34,22 @@ app.get('/api/info', (request, response) => {
     <h1>The phonebook has ${phonebook.length} people</h1>
     <h1>${new Date()}</h1>    
     `)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const note = persons.find(p => p.id === id)
+
+    if (note) {
+        response.json(note)
+    }
+    else { response.status(404).end() }
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    persons = persons.filter(p => p.id !== id)
+    response.status(204).end()
 })
 
 app.listen(3001, () => {
