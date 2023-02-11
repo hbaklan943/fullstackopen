dd0aec4f402cfc8a28f062681d75e9d081aa92a2
hbaklan943 <hbaklan943@gmail.com>
2023-02-11 17:32:12 +0300

    exercise3.5

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -52,6 +52,25 @@ app.delete('/api/persons/:id', (request, response) => {
     response.status(204).end()
 })
 
+app.post('/api/persons', (request, response) => {
+    const id = Number(request.params.id)
+    const body = request.body
+
+    if (!body.name || !body.number) {
+        return response.status(400).json({
+            error: 'name or number missing',
+        })
+    }
+    const newPerson = {
+        "id": Math.floor(Math.random() * 200),
+        "name": body.name,
+        "number": body.number
+    }
+    persons = persons.concat(newPerson)
+    console.log(persons);
+    response.json(newPerson)
+})
+
 app.listen(3001, () => {
     console.log("server is running");
 })
