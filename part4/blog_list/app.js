e45ab2aa976a377363b5a6157c0a37ed6dccd557
hbaklan943 <hbaklan943@gmail.com>
2023-02-20 16:30:12 +0300

    exercise_4.2

--- part4/blog_list/app.js
+++ part4/blog_list/app.js
@@ -1,4 +1,25 @@
+const config = require('./utils/config')
 const http = require('http')
 const express = require('express')
+const cors = require('cors')
+const mongoose = require('mongoose')
 
 const app = express()
+const blogsRouter = require('./controllers/blogs')
+
+const mongoUrl = config.MONGODB_URI
+
+mongoose.connect(mongoUrl)
+  .then(() => {
+    console.log('connected to mongodb');
+  })
+
+app.use(cors())
+app.use(express.json())
+app.use('/api/blogs', blogsRouter)
+
+
+
+
+
+module.exports = app
