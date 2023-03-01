f79cb5229bfca5c9f44571ee2338d08e85a03b8b
hbaklan943 <hbaklan943@gmail.com>
2023-03-01 19:53:00 +0300

    exercise4.15

--- part4/blog_list/app.js
+++ part4/blog_list/app.js
@@ -1,11 +1,14 @@
 const config = require('./utils/config')
 const http = require('http')
 const express = require('express')
+require('express-async-errors')
 const cors = require('cors')
 const mongoose = require('mongoose')
 
 const app = express()
 const blogsRouter = require('./controllers/blogs')
+const usersRouter = require('./controllers/users')
+const middleware = require('./utils/middleware')
 
 const mongoUrl = config.MONGODB_URI
 
@@ -16,10 +19,13 @@ mongoose.connect(mongoUrl)
 
 app.use(cors())
 app.use(express.json())
+app.use(middleware.requestLogger)
+
 app.use('/api/blogs', blogsRouter)
+app.use('/api/users', usersRouter)
 
-
-
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
 
 
 module.exports = app
