05fdf245550980e4409f1e3de46ccad894b6f818
hbaklan943 <hbaklan943@gmail.com>
2023-03-16 02:31:35 +0300

    exercise6.3

--- part6/note_redux/src/reducers/noteReducer.js
+++ part6/note_redux/src/reducers/noteReducer.js
@@ -3,13 +3,33 @@ const noteReducer = (state = [], action) => {
     case 'NEW_NOTE': {
       return [...state, action.payload]
     }
-
     case 'TOGGLE_IMPORTANCE': {
       const id = action.payload.id
       const noteToChange = state.find(note => note.id === id)
       return state.map(note => note.id !== id ? note : { ...note, important: !noteToChange.important })
     }
     default: return state
+  }
+}
+
+const generateId = () => {
+  console.log(Math.floor(Math.random() * 10000000));
+  return Math.floor(Math.random() * 10000000)
+}
+export const createNote = (content) => {
+  return {
+    type: 'NEW_NOTE',
+    payload: {
+      content,
+      important: false,
+      id: generateId()
+    }
+  }
+}
+export const toggleImportanceOf = (id) => {
+  return {
+    type: 'TOGGLE_IMPORTANCE',
+    payload: { id }
   }
 }
 
--- part6/redux-anecdotes/src/index.js
+++ part6/redux-anecdotes/src/index.js
@@ -1,14 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
+import App from './App'
+import reducer from './reducers/anecdoteReducer'
 
-import App from './app'
-import noteReducer from './reducers/noteReducer'
-
-const store = createStore(noteReducer)
-
-
+const store = createStore(reducer)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
